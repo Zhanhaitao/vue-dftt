93f53b06cc1fadc26fab8f2229bcd92ed058acf5
zhanhaitao <13564420419@163.com>
2019-04-04 15:11:45 +0800

    配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:prettier/recommended'],
+  plugins: ['vue'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 2,
+        semi: true
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 };
